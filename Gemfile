d2042849cc5a3ac996ceb8daff91c6583d186835
Ryan Cobb <ryan.david.cobb@gmail.com>
2017-09-04 19:33:53 -0600

    try cap again

--- Gemfile
+++ Gemfile
@@ -55,12 +55,11 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'rspec-rails', '~> 3.5'
-  gem 'capistrano', '~> 3.6'
-  gem 'capistrano-rails', '~> 1.3'
-  gem 'capistrano3-puma'
-  gem 'capistrano-rbenv'
-  gem 'capistrano3-nginx'
-  gem 'capistrano-upload-config'
+  gem 'capistrano',         require: false
+  gem 'capistrano-rbenv',   require: false
+  gem 'capistrano-rails',   require: false
+  gem 'capistrano-bundler', require: false
+  gem 'capistrano3-puma',   require: false
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,46 +1,40 @@
-# config valid only for current version of Capistrano
-lock "3.9.0"
+set :application, 'KslNotifier'
+set :repo_url, 'git@github.com:ryancobb/ksl_notifier.git'
+set :user,            'deploy'
+set :puma_threads,    [4, 16]
+set :puma_workers,    1
 
-server '45.76.70.99', :user => 'deploy', :roles => [:web, :app, :db], :primary => true
+# Don't change these unless you know what you're doing
+set :pty,             true
+set :use_sudo,        false
+set :stage,           :production
+set :deploy_via,      :remote_cache
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
-set :application, "KslNotifier"
-set :repo_url, "git@github.com:ryancobb/ksl_notifier.git"
+## Defaults:
+set :scm,           :git
+set :branch,        :master
+set :format,        :pretty
+set :log_level,     :debug
+set :keep_releases, 5
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml", "config/secrets.yml"
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+## Linked Files & Directories (Default None):
+set :linked_files, %w{config/database.yml}
+set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
   task :make_dirs do
-    on roles(:app) do 
+    on roles(:app) do
       execute "mkdir #{shared_path}/tmp/sockets -p"
       execute "mkdir #{shared_path}/tmp/pids -p"
     end
@@ -61,7 +55,7 @@ namespace :deploy do
     end
   end
 
-  desc "Initial Deploy"
+  desc 'Initial Deploy'
   task :initial do
     on roles(:app) do
       before 'deploy:restart', 'puma:start'
@@ -69,14 +63,15 @@ namespace :deploy do
     end
   end
 
-  desc "Restart application"
+  desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       invoke 'puma:restart'
     end
   end
 
-  before 'puma:config'
-  before 'puma:nginx_config'
-  after 'puma:start_restart', 'nginx:restart'
+  before :starting,     :check_revision
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  after  :finishing,    :restart
 end
