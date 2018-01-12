3fb8ba7f94aa6faa753dfc927cb7100eb073b68f
Ryan Cobb <ryan.cobb@mx.com>
2018-01-12 10:18:10 -0700

    clean up email worker

--- app/workers/user_notification_email_worker.rb
+++ app/workers/user_notification_email_worker.rb
@@ -7,8 +7,6 @@ class UserNotificationEmailWorker
 
     users.find_each do |user|
       ::UserMailer.notification_email(user).deliver_now
-
-      ::UserMailer.deliver_notification_email
     end
   end
 end
