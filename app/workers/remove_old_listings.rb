43241ef29b5b04d2a301eb2d228309418367e4fc
Ryan Cobb <ryan.david.cobb@gmail.com>
2018-02-04 18:17:49 -0700

    keep listings for 7 days

--- app/workers/remove_old_listings.rb
+++ app/workers/remove_old_listings.rb
@@ -2,7 +2,7 @@ class RemoveOldListings
   include Sidekiq::Worker
 
   def perform
-    listings_to_destroy = ::Listing.where('created_at < ?', 1.day.ago).select(:id)
+    listings_to_destroy = ::Listing.where('created_at < ?', 7.day.ago).select(:id)
 
     listings_to_destroy.find_in_batches do |batch|
       ids = batch.map(&:id)
